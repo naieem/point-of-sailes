b33a3a03fa7625186353dac89f516ab538e0c498
unknown <naieemsupto@gmail.com>
2021-02-09 19:10:04 +0600

    setup page checkup

--- setup_page.php
+++ setup_page.php
@@ -1,79 +1,6 @@
 <?php
 ob_start();
 session_start();
-/*if (isset($_POST['host']) and isset($_POST['username']) and $_POST['host'] != "" and $_POST['username'] != "") {
-    $host = trim($_POST['host']);
-    $user = trim($_POST['username']);
-    $pass = trim($_POST['password']);
-    $name = '';
-    $_SESSION['host'] = $host;
-    $_SESSION['user'] = $user;
-    $_SESSION['pass'] = $pass;
-    $con = mysqli_connect("$host", "$user", "$pass");
-    if (!$con) {
-        $data = "Database Configration is Not vaild";
-        header("location: install_step1.php?msg=$data");
-        exit;
-    }
-    if (isset($_POST['name'])) {
-        $name = $_POST['name'];
-        $sql = "CREATE DATABASE $name";
-        if (!mysqli_query($con, $sql)) {
-            $data = "This Database Name Is Already In the DataBase";
-            header("location: install_step2.php?msg=$data");
-            exit;
-        }
-    }
-    if (isset($_POST['select_box'])) {
-        $name = $_POST['select_box'];
-    }   
-    $_SESSION['db_name'] = $name;
-    $conn = mysqli_connect("$host", "$user", "$pass", "$name");
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-    if (isset($_POST['dummy'])) {
-        $sql = file_get_contents('install_db/structure_with_demo.sql');
-    } else {
-        $sql = file_get_contents('install_db/structure_only.sql');
-    }
-    // var_dump($host,$user,$pass,$name);
-    // /* execute multi query */
-    /*if (mysqli_multi_query($conn, $sql)) {
-        do {
-            try {
-                if ($result = mysqli_store_result($conn)) {
-                    while ($row = mysqli_fetch_row($result)) {
-                        printf("%s\n", $row[0]);
-                    }
-                    mysqli_free_result($result);
-                } else {
-                    die("issue with query")
-                //    echo "issue with query";
-                }
-                /* print divider 
-                if (mysqli_more_results($conn)) {
-                    printf("-----------------\n");
-                }
-            } catch (Exception $e) {
-                echo 'Caught exception: ', $e->getMessage(), "\n";
-            }
-        } while (mysqli_next_result($conn));
-    } else {
-        die('Problem in query execution.');
-    }
-    /*$ourFileName = "config.php";
-    $ourFileHandle = fopen($ourFileName, 'w') or die("Not able to write config file (check directory permissions). You can directly Create config.php file as like config.php.sample file. ");
-    $data = '<?php $config["database"] = "' . $name . '"; $config["host"]= "' . $host . '";$config["username"]= "' . $user . '"; $config["password"]= "' . $pass . '";?>';
-    fwrite($ourFileHandle, $data);
-    fclose($ourFileHandle);
-    // mysqli_close($con);
-    header("location: install_step3.php");*/
-
-// } else {
-//     header("location: install_step1.php");
-// }
-//
 
 if (isset($_POST['host']) and isset($_POST['username']) and $_POST['host'] != "" and $_POST['username'] != "") {
     $host = trim($_POST['host']);
@@ -90,13 +17,6 @@ if (isset($_POST['host']) and isset($_POST['username']) and $_POST['host'] != ""
     $_SESSION['user'] = $user;
     $_SESSION['pass'] = $pass;
     $_SESSION['db_name'] = $name;
-    // $link = mysqli_connect("$host", "$user", "$pass");
-    // if (!$link) {
-    //     $data = "Database Configration is Not vaild";
-    //     header("location: install_step1.php?msg=$data");
-    //     exit;
-    // }
-
     $con = mysqli_connect("$host", "$user", "$pass");
 // Check connection
     if (isset($_POST['name'])) {
@@ -147,7 +67,7 @@ if (isset($_POST['host']) and isset($_POST['username']) and $_POST['host'] != ""
         die('Problem in query execution.');
     }
 
-
+    mysqli_close($con);
     $ourFileName = "config.php";
     $ourFileHandle = fopen($ourFileName, 'w') or die("Not able to write config file (check directory permissions). You can directly Create config.php file as like config.php.sample file. ");
     $data = '<?php $config["database"] = "' . $name . '"; $config["host"]= "' . $host . '";$config["username"]= "' . $user . '"; $config["password"]= "' . $pass . '";?>';
