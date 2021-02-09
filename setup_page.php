3e2f39da62be01a368ca9b93cf338f531976cfde
unknown <naieemsupto@gmail.com>
2021-02-09 18:51:23 +0600

    setup page checkup

--- setup_page.php
+++ setup_page.php
@@ -63,12 +63,12 @@ if (isset($_POST['host']) and isset($_POST['username']) and $_POST['host'] != ""
     } else {
         die('Problem in query execution.');
     }
-    mysqli_close($con);
     $ourFileName = "config.php";
     $ourFileHandle = fopen($ourFileName, 'w') or die("Not able to write config file (check directory permissions). You can directly Create config.php file as like config.php.sample file. ");
     $data = '<?php $config["database"] = "' . $name . '"; $config["host"]= "' . $host . '";$config["username"]= "' . $user . '"; $config["password"]= "' . $pass . '";?>';
     fwrite($ourFileHandle, $data);
     fclose($ourFileHandle);
+    mysqli_close($con);
     header("location: install_step3.php");
 
 } else {
